938a20825e34df630326cbe362f4d5449f893d03
petecarapetyan <pete@datafundamentals.com>
2021-08-09 16:51:44 -0500

    WIP

--- test/my.test.js
+++ test/my.test.js
@@ -6,8 +6,10 @@ const { test } = require('mocha')
 const MY_PROJECT_ID = "fauxmazon"
 const MY_UID = "user_abc"
 const THEIR_UID = "user_xyz"
+const MOD_UID = "user_mod"
 const MY_AUTH = {uid: MY_UID, email: "abc@gmail.com"}
 const THEIR_AUTH = {uid: THEIR_UID, email: "xyz@gmail.com"}
+const MOD_AUTH = {uid: MOD_UID, email: "mod@gmail.com", isModerator: true}
 const PUBLIC_POST_ID = "public_post"
 const PRIVATE_POST_ID = "private_post"
 
@@ -101,6 +103,92 @@ describe ("my first test", () => {
     await firebase.assertSucceeds(testRead.get())
   })
 
+
+  it("allows a user to edit their own post", async () => {
+    const POST_ID = "post_123";
+    const admin = getAdminFirestore();
+    await admin.collection("posts").doc(POST_ID).set({content: "before", authorId: MY_UID})
+    const db = getFirestore(MY_AUTH)
+    const testDoc = db.collection("posts").doc(POST_ID)
+    await firebase.assertSucceeds(testDoc.update({content: "after"}));
+  })
+
+  it("does not allow a user to edit someone else's post", async () => {
+    const POST_ID = "post_123";
+    const admin = getAdminFirestore();
+    await admin.collection("posts").doc(POST_ID).set({content: "before", authorId: MY_UID})
+    const db = getFirestore(THEIR_AUTH)
+    const testDoc = db.collection("posts").doc(POST_ID)
+    await firebase.assertFails(testDoc.update({content: "after"}));
+  })
+
+  it("allows a moderator to edit their own post", async () => {
+    const POST_ID = "post_123";
+    const admin = getAdminFirestore();
+    await admin.collection("posts").doc(POST_ID).set({content: "before", authorId: MY_UID})
+    const db = getFirestore(MOD_AUTH)
+    const testDoc = db.collection("posts").doc(POST_ID)
+    await firebase.assertSucceeds(testDoc.update({content: "after"}));
+  })
+
+  it("allows a user to edit their own room post", async () => {
+    const POST_ID = "post_123";
+    const postPath = `/rooms/room_abc/posts/${POST_ID}`
+    const admin = getAdminFirestore();
+    await admin.doc(postPath).set({content: "before", authorId: MY_UID})
+    const db = getFirestore(MY_AUTH)
+    const testDoc = db.doc(postPath)
+    await firebase.assertSucceeds(testDoc.update({content: "after"}));
+  })
+
+  it("prevents different user to editing another's own room post", async () => {
+    const POST_ID = "post_123";
+    const postPath = `/rooms/room_abc/posts/${POST_ID}`
+    const admin = getAdminFirestore();
+    await admin.doc(postPath).set({content: "before", authorId: THEIR_UID})
+    const db = getFirestore(MY_AUTH)
+    const testDoc = db.doc(postPath)
+    await firebase.assertFails(testDoc.update({content: "after"}));
+  })
+
+  it("allows room mod to edit another's room post", async () => {
+    const ROOM_PATH = "rooms/room_abc";
+    const POST_PATH = `${ROOM_PATH}/posts/post_123`;
+    const admin = getAdminFirestore();
+    await admin.doc(ROOM_PATH).set({topic: "Unit testers", roomMods: [MY_UID, "joeblo"]});
+    await admin.doc(POST_PATH).set({content: "before", authorId: THEIR_UID});
+    const db = getFirestore(MY_AUTH)
+    const testDoc = db.doc(POST_PATH)
+    await firebase.assertSucceeds(testDoc.update({content: "after"}));
+  })
+
+  it("create allowed when author listed is same as user", async () => {
+    const POST_PATH = `posts/post_123`;
+    const db = getFirestore(MY_AUTH)
+    const testDoc = db.doc(POST_PATH)
+    await firebase.assertSucceeds(testDoc.set({content: "some content", authorId: MY_UID, visibility: "public", headline: "Interesting Post"}));
+  })
+
+  // it("create not allowed when author listed is not same as user", async () => {
+  //   const POST_PATH = `posts/post_123`;
+  //   const db = getFirestore(THEIR_AUTH)
+  //   const testDoc = db.doc(POST_PATH)
+  //   await firebase.assertFails(testDoc.set({content: "some content", authorId: MY_UID, visibility: "public", headline: "Interesting Post"}));
+  // })
+
+  // it("cannot create missing some required fields", async () => {
+  //   const POST_PATH = `posts/post_123`;
+  //   const db = getFirestore(MY_AUTH)
+  //   const testDoc = db.doc(POST_PATH)
+  //   await firebase.assertFails(testDoc.set({content: "Lorem Ipsum", authorId: MY_UID, headline: "Interesting Post"}));
+  // })
+
+  // it("can create when have required fields", async () => {
+  //   const POST_PATH = `posts/post_123`;
+  //   const db = getFirestore(MY_AUTH)
+  //   const testDoc = db.doc(POST_PATH)
+  //   await firebase.assertSucceeds(testDoc.set({content: "Lorem Ipsum", authorId: MY_UID, visibility: "public", headline: "Interesting Post"}));
+  // })
 })
 
 after( async() => {
